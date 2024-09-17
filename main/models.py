12db31a0fbd26a375741088cb9deb13df4719fe3
MakarimZufar <makarim.zufar@gmail.com>
2024-09-17 13:39:29 +0700

    solve typo and validate stock can't negative

--- main/models.py
+++ main/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Product(models.Model):
     name = models.CharField(max_length=225)
-    stock = models.IntegerField(default=0)
+    stock = models.PositiveIntegerField(default=0)
     category = models.CharField(max_length=225,default="uncategorized")
     price = models.IntegerField()
     description = models.TextField()

55390466309189a41cd91a7d8b221dbe0a599787
MakarimZufar <makarim.zufar@gmail.com>
2024-09-18 08:49:49 +0700

    memperbaiki urls dan model agar merandom pk dengan uuid

--- main/models.py
+++ main/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+import uuid
 
 # Create your models here.
 class Product(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=225)
     stock = models.PositiveIntegerField(default=0)
     category = models.CharField(max_length=225,default="uncategorized")
--- main/urls.py
+++ main/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('', show_main, name="show_main"),
     path('add_product/', add_product, name="add_product"),
     path('xml/', show_xml, name="show_xml"),
-    path('xml/<int:id>/', show_xml_by_id, name="show_xml_by_id"),
+    path('xml/<str:id>/', show_xml_by_id, name="show_xml_by_id"),
     path('json/', show_json, name="show_json"),
-    path('json/<int:id>/', show_json_by_id, name="show_json_by_id"),
+    path('json/<str:id>/', show_json_by_id, name="show_json_by_id"),
 ]
